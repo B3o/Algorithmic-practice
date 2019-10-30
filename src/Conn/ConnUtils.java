840262dfaa2094a55456826d348c2ab1b3bf6523
Bmo <84262602@qq.com>
2019-10-30 16:25:38 +0800

    工具类

---
+++ src/Conn/ConnUtils.java
@@ -0,0 +1,13 @@
+package Conn;
+
+/**
+ * @author Bmo
+ * @date 2019/10/30 15:57
+ */
+public class ConnUtils {
+    public static void swap(int [] arr, int i, int j) {
+        int tmp = i;
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
